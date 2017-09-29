758cd691c8d50ce6c2dcd6dc8482b77a49fccd30
wylanneely <wylanneely@gmail.com>
2017-09-29 16:34:37 -0700

    basics, mlfile was a little messed up, working custom

--- BasicCoreML/BasicCoreML/ViewController.swift
+++ BasicCoreML/BasicCoreML/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let model = GoogLeNetPlaces()
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        let imageView = sender.view as? UIImageView
+        
+        if let imageToAnalyze = imageView?.image {
+            if let sceneString = sceneLabel(forImage: imageToAnalyze) {
+                categoryLabel.text = sceneString
+            }
+        }
+        
+        
+    }
+    
+    func sceneLabel (forImage image : UIImage) -> String? {
+        
+        if let pixelBuffer = ImageProcessor.pixelBuffer(forIMage: image.cgImage!) {
+            
+            
+            guard let scene = try? model.prediction(sceneImage: pixelBuffer) else {
+                fatalError("Unexpected Error")}
+            return scene.sceneLabel
+            }
+        return nil
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
